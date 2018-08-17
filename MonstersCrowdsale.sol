dda2183c28514e717cfa157f0fb86973fad4c376
Artur Teregulov <info@teregulov.com>
2018-08-17 23:06:21 +0300

    update cap

--- MonstersCrowdsale.sol
+++ MonstersCrowdsale.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.24;
 import "./FinalizableCrowdsale.sol";
 
 contract MonstersGameXTokenCrowdsale is FinalizableCrowdsale {
-  uint256 public cap_ = 1000000;
-  uint256 public openingTime_ = 1534377600;
-  uint256 public closingTime_ = 1542326400;
+  uint256 public cap_ = 1500000000000000000000;
+  uint256 public openingTime_ = 1534377600; // Thu, 16 Aug 2018 00:00:00 GMT
+  uint256 public closingTime_ = 1542326400; // Fri, 16 Nov 2018 00:00:00 GMT
 
 
   constructor(uint256 _rate, address _wallet, address _token) public
