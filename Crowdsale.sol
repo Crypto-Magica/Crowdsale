4d23fee02221c9dd4bac8a024bb93d655cdd3219
Artur Teregulov <info@teregulov.com>
2018-08-17 21:25:35 +0300

    update token type

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -20,7 +20,7 @@ import "./MonstersToken.sol";
  */
 contract Crowdsale {
   using SafeMath for uint256;
-  using SafeERC20 for ERC20;
+  using SafeERC20 for MonstersGameXToken;
 
   // The token being sold
   MonstersGameXToken public token;
