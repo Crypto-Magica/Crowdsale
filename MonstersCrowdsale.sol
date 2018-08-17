f091b9d1123029a93a33cf19af2af98e3d866e2d
Vitaliy <vitaliy.lebedev7@gmail.com>
2018-08-18 00:08:12 +0600

    fix name

--- MonstersCrowdsale.sol
+++ MonstersCrowdsale.sol
@@ -6,7 +6,7 @@ import "./FinalizableCrowdsale.sol";
 contract MonstersGameXTokenCrowdsale is FinalizableCrowdsale {
     
   constructor(uint256 _rate, address _wallet, address _token) public
-    FinalizableCrowdsale(_rate, _wallet, MonsterBitToken(_token)) {
+    FinalizableCrowdsale(_rate, _wallet, MonstersGameXTokenCrowdsale(_token)) {
   }
   
   function setRate(uint256 newRate) public onlyOwner {
