c8ed65adf5de36a78ca7a348fd9965582f943c06
Vitaliy <vitaliy.lebedev7@gmail.com>
2018-08-18 00:05:58 +0600

    set times

--- MonstersCrowdsale.sol
+++ MonstersCrowdsale.sol
@@ -5,8 +5,8 @@ import "./FinalizableCrowdsale.sol";
 
 contract MonstersGameXTokenCrowdsale is FinalizableCrowdsale {
     
-  function MonstersGameXTokenCrowdsale(uint256 _rate, address _wallet, address _token, uint256 _openingTime, uint256 _closingTime) public
-    FinalizableCrowdsale(_rate, _wallet, MonsterBitToken(_token),  _openingTime, _closingTime) {
+  constructor(uint256 _rate, address _wallet, address _token) public
+    FinalizableCrowdsale(_rate, _wallet, MonsterBitToken(_token)) {
   }
   
   function setRate(uint256 newRate) public onlyOwner {
