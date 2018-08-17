b0aea11283714bf182cc46df1638068962b76966
Artur Teregulov <info@teregulov.com>
2018-08-17 18:42:13 +0300

    Add own crowdsale

---
+++ MonstersCrowdsale.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+import "./FinalizableCrowdsale.sol";
+
+
+contract MonstersCrowdsale is FinalizableCrowdsale {
+    
+  function MonsterTokenCrowdsale(uint256 _rate, address _wallet, address _token, uint256 _openingTime, uint256 _closingTime) public
+    FinalizableCrowdsale(_rate, _wallet, MonsterBitToken(_token),  _openingTime, _closingTime) {
+  }
+  
+  function setRate(uint256 newRate) public onlyOwner {
+      rate = newRate;
+  }
+  
+  function sendTokens(address beneficiary, uint256 tokensAmount) public onlyOwner {
+    require(beneficiary != address(0));
+    _processPurchase(beneficiary, tokensAmount);
+    TokenSending(beneficiary, tokensAmount); // event
+  }
+}
